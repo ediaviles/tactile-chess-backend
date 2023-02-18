1793299b45dcf3b178984fdf8bac695654016cdc
Edison Aviles <eaviles@andrew.cmu.edu>
2023-02-17 22:23:45 -0500

    added functions to seek a game and make moves

--- index.js
+++ index.js
@@ -5,14 +5,36 @@ import https from "https";
 import "dotenv";
 import fetch from "node-fetch";
 import http from "http";
+import "querystring";
 
 
 const app = express();
 app.use(cors());
 
-app.get("/getData", (req, res) => {
-    res.send("hello");
-})
+const readStream = processLine => response => {
+    const matcher = /\r?\n/;
+    const decoder = new TextDecoder();
+    let buf = '';
+    return new Promise((resolve, fail) => {
+        response.body.on('data', v => {
+            const chunk = decoder.decode(v, {stream: true});
+            buf += chunk; // this chunk object is a string of our JSON object event
+            const parts = buf.split(matcher);
+            buf = parts.pop();
+            for (const i of parts.filter(p => p)) processLine(JSON.parse(i));
+        });
+        response.body.on('end', () => {
+            if (buf.length > 0) processLine(JSON.parse(buf));
+            resolve();
+        })
+        response.body.on('error', fail);
+    })
+}
+const onMessage = obj => console.log(obj);
+
+const headers = {
+    Authorization: 'Bearer ' + 'lip_Zt6rLGHWhZj8qcaeTaLG'
+};
 
 app.post("/seekGame", (req, res) => {
     // Initially open a listening port
@@ -20,50 +42,27 @@ app.post("/seekGame", (req, res) => {
     const optionsStream = {
         headers: headers,
         host: 'lichess.org',
-        path: '/api/stream/event',
-        method: 'GET'
+        path: '/api/stream/event'
     }
-    const streamReq = https.request(optionsStream, (streamRes) => {
-        console.log(streamRes)
-    })
-    // Create the seek and wait for match to start
+    const stream = fetch('https://lichess.org/api/stream/event', {headers: headers});
+    stream
+        .then(readStream(onMessage));
+    console.log('outside');
+    fetch('https://lichess.org/api/board/seek', {method: 'POST', headers: headers});
 } )
 
-const headers = {
-    Authorization: 'Bearer ' + 'lip_Zt6rLGHWhZj8qcaeTaLG'
-};
-const options = {
-    host: 'lichess.org',
-    path: '/api/board/game/HKORenq4/move/b2b3',
-    headers: {
-        Authorization: 'Bearer ' + 'lip_Zt6rLGHWhZj8qcaeTaLG'
-    },
-    method: 'POST'
-}
-
-let gameId = "inputGameId";
-let movesList = [
-    "a2a3",
-    "b2b3",
-    "c2c3"
-];
-
-/*const req = https.request(options, (res) => {
-    console.log(res.statusCode)
-    console.log(res)
-});
-req.on('error', (e) => {
-    console.log(e.message)
+app.post("/makeMove/:gameId/:move", (req, res) => {
+    let gameId = req.params.gameId;
+    let move = req.params.move;
+    console.log("got called");
+    try {
+        fetch(`https://lichess.org/api/board/game/${gameId}/move/${move}`, {method: 'POST', headers: headers}).then((e) => console.log(e.status));
+        res.json({msg: 'success'})
+    } catch (e) {
+        console.log(e.status)
+    }
 })
-req.end()*/
+
 app.listen(5000, async ()=> {
     console.log('app is running')
 })
-
-/*fetch('https://lichess.org/api/stream/game/yxQMAb4R', { headers })
-    .then((res) => {
-        res.on('data', function(state) {
-            console.log(state);
-        })
-    })
-    .then(console.log);*/
